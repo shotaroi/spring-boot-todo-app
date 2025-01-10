82944cb03ac5cca8706e4e681afedd4dd401c4a7
shotaroi <58347058+shotaroi@users.noreply.github.com>
2025-01-10 19:48:40 +0100

    Implement ToDoRepository

--- spring-boot-todo-app/src/main/java/com/shotaroi/springboottodoapp/repository/ToDoRepository.java
+++ spring-boot-todo-app/src/main/java/com/shotaroi/springboottodoapp/repository/ToDoRepository.java
@@ -1,4 +1,9 @@
 package com.shotaroi.springboottodoapp.repository;
 
-public interface ToDoRepository {
+import com.shotaroi.springboottodoapp.model.Todo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ToDoRepository extends JpaRepository<Todo, Long> {
 }
