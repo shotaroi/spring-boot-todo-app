323944b29de1bd45845246c14b7f54a8fd736a18
shotaroi <58347058+shotaroi@users.noreply.github.com>
2025-01-10 19:41:54 +0100

    Implement Todo model

--- spring-boot-todo-app/src/main/java/com/shotaroi/springboottodoapp/model/Todo.java
+++ spring-boot-todo-app/src/main/java/com/shotaroi/springboottodoapp/model/Todo.java
@@ -1,4 +1,19 @@
 package com.shotaroi.springboottodoapp.model;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import lombok.Data;
+
+@Data
 public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private boolean completed;
+
+    
+
 }
